076eeb38ac71f0087d0b88d3f75582e68e638886
dgulczynski <dominik.gulczynski@gmail.com>
2020-04-07 18:51:21 +0200

    Add examples

--- hydrogen.ml
+++ hydrogen.ml
@@ -26,8 +26,7 @@ let rec string_of_expr (e : expr) : string =
         | I i -> string_of_int i
         | V v -> v
         | _ -> "(" ^ string_of_expr e' ^ ")" )
-      ^ " "
-      ^
+      ^ " " ^
       match e'' with
       | I i -> string_of_int i
       | V v -> v
@@ -108,14 +107,14 @@ let some_terms =
     Lam ("x", App (V "x", I 2));
     Lam ("x", App (V "x", V "x"));
     Lam ("y", App (Lam ("x", V "x"), I 1));
-    Lam ("y", App (Lam ("x", App (V "y", V "x")), I 1));
     Lam ("x", Lam ("y", V "x"));
+    Lam ("y", App (Lam ("x", App (V "y", V "x")), I 1));
     Lam ("x", App (Lam ("y", V "y"), App (V "x", I 42)));
     Lam ("x", Lam ("y", Lam ("z", App (App (V "x", V "z"), App (V "y", V "z")))));
     Let ("f", Lam("x", App(V "x", I 1)), Lam ("y", App (V "f", Lam ("x", App (V "y", V "x")))));
     Let ("g", Lam ("x", App(V "x", App(V "x", I 1))), Let ("f", Lam("x", App(V "x", I 1)), Lam ("y", App(V "g", App (V "f", Lam ("x", App (V "y", V "x")))))));
-    Fun("fix", "f", App(V "f", App(V "fix", V "f")));
     Fun("f", "x", App(V "f", App(V "f", I 1)));
+    Fun("fix", "f", App(V "f", App(V "fix", V "f")));
     App(Fun("fix", "f", App(V "f", App(V "fix", V "f"))), Lam("x", Lam("y", Lam ("z", I 2))));
   ]
 
