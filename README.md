c78791b134bfb898b05e630335b5ea9e464384a1
dgulczynski <dominik.gulczynski@gmail.com>
2020-04-16 18:23:50 +0200

    Add type generalization on top level expressions

--- README.md
+++ README.md
@@ -8,23 +8,24 @@ Type inference playground.
 
 <img src="https://render.githubusercontent.com/render/math?math=\text{type} \ni \tau \Coloneqq \alpha \mid \text{Int} \mid \tau \rightarrow \tau">
 
-<img src="https://render.githubusercontent.com/render/math?math=\text{expr} \ni e \Coloneqq x \mid n \mid \lambda x . e \mid \text{fun} f x . e \mid e e \mid \text{let} x = e \text{ in} e">
+<img src="https://render.githubusercontent.com/render/math?math=\text{expr} \ni e \Coloneqq x \mid n \mid \lambda x . e \mid \text{fun} f x . e \mid e \: e \mid \text{let} x = e \: \text{in} \: e">
 
 # Usage
 Simply running `ocaml hydrogen.ml` should result in output like:
 ```
-Type of λx. x is t0 -> t0
-Type of λx. x 2 is (Int -> t1) -> t1
-ERROR: Cannot construct infinite type t0 ~> t0 -> t1
+Type of λx. x is 't0 -> 't0
+Type of λx. x 2 is (Int -> 't1) -> 't1
+ERROR: Cannot construct infinite type t0 ~ t0 -> t1
 Type of λx. x x is ILL-TYPED
-Type of λy. (λx. x) 1 is t0 -> Int
-Type of λx. λy. x is t0 -> t1 -> t0
-Type of λy. (λx. y x) 1 is (Int -> t3) -> t3
-Type of λx. (λy. y) (x 42) is (Int -> t3) -> t3
-Type of λx. λy. λz. (x z) (y z) is (t2 -> t4 -> t5) -> (t2 -> t4) -> t2 -> t5
-Type of let f = λx. x 1 in λy. f (λx. y x) is (Int -> t5) -> t5 with env: (f : (Int -> t1) -> t1)
-Type of let g = λx. x (x 1) in let f = λx. x 1 in λy. g (f (λx. y x)) is (Int -> Int -> Int) -> Int with env: (f : (Int -> t4) -> t4) (g : (Int -> Int) -> Int)
+Type of λy. (λx. x) 1 is 't0 -> Int
+Type of λx. λy. x is 't0 -> 't1 -> 't0
+Type of λy. (λx. y x) 1 is (Int -> 't3) -> 't3
+Type of λx. (λy. y) (x 42) is (Int -> 't3) -> 't3
+Type of λx. λy. λz. (x z) (y z) is ('t2 -> 't4 -> 't5) -> ('t2 -> 't4) -> 't2 -> 't5
+Type of let f = λx. x 1 in λy. f (λx. y x) is (Int -> 't6) -> 't6 with env: (f : (Int -> 't1) -> 't1)
+Type of let g = λx. x (x 1) in let f = λx. x 1 in λy. g (f (λx. y x)) is (Int -> Int -> Int) -> Int with env: (f : (Int -> 't4) -> 't4) (g : (Int -> Int) -> Int)
 Type of fun f x. f (f 1) is Int -> Int
-Type of fun fix f. f (fix f) is (t2 -> t2) -> t2
-Type of (fun fix f. f (fix f)) (λx. λy. λz. 2) is t5 -> t6 -> Int
+Type of fun fix f. f (fix f) is ('t2 -> 't2) -> 't2
+Type of (fun fix f. f (fix f)) (λx. λy. λz. 2) is 't5 -> 't6 -> Int
+Type of let id = λx. x in id id is 't2 -> 't2 with env: (id : 't0 -> 't0)
 ```
--- hydrogen.ml
+++ hydrogen.ml
@@ -10,7 +10,7 @@ type expr =
   | Let of var * expr * expr
   | Fun of var * var * expr
 
-type t = Int | Arrow of t * t | TV of tvar | Bad
+type t = Int | Arrow of t * t | TV of tvar | GV of tvar | Bad
 
 let rec string_of_type (t : t) : string =
   match t with
@@ -20,6 +20,7 @@ let rec string_of_type (t : t) : string =
       | Arrow _ -> "(" ^ string_of_type t1 ^ ") -> " ^ string_of_type t2
       | _ -> string_of_type t1 ^ " -> " ^ string_of_type t2 )
   | TV tv -> tv
+  | GV tv -> "'" ^ tv
   | Bad -> "ILL-TYPED"
 
 let rec string_of_expr (e : expr) : string =
@@ -46,7 +47,7 @@ let infer_type (expr : expr) =
   : (var * t) list * t * (t * t) list =
     match expr with
     | I _ -> (gamma, Int, cs)
-    | V v -> (gamma, snd (List.find (fun (v', t') -> v' = v) gamma), cs)
+    | V v -> (gamma, instantiate (snd (List.find (fun (v', t') -> v' = v) gamma)), cs)
     | Lam (x, e) ->
         let tx = newTV () in
         let _, te, cse = gather_constraints ((x, tx) :: gamma) e cs in
@@ -65,7 +66,13 @@ let infer_type (expr : expr) =
         (gamma, tfx, (tf, Arrow (tx, tfx)) :: csx)
     | Let (x, e, e') ->
         let _, te, cse = gather_constraints gamma e cs in
-        gather_constraints ((x, unify_types te cse) :: gamma) e' cs
+        gather_constraints
+          ( ( x,
+              generalize
+                (free_type_variables (List.map snd gamma))
+                (unify_types te cse) )
+          :: gamma )
+          e' cs
   
   and unify_types (t : t) (cs : (t * t) list) : t =
     match cs with
@@ -75,11 +82,11 @@ let infer_type (expr : expr) =
     | (TV a, t2) :: cs ->
         if TV a = t2 then unify_types t cs
         else if occurs (TV a) t2 then (
-          Printf.printf "ERROR: Cannot construct infinite type %s ~> %s\n" a
+          Printf.printf "ERROR: Cannot construct infinite type %s ~ %s\n" a
             (string_of_type t2);
           Bad )
         else
-          let substitute = subst (TV a) t2 in
+          let substitute = subst a t2 in
           unify_types (substitute t)
             (List.map (fun (c1, c2) -> (substitute c1, substitute c2)) cs)
     | (t1, TV a) :: cs -> unify_types t ((TV a, t1) :: cs)
@@ -91,10 +98,11 @@ let infer_type (expr : expr) =
             (string_of_type t1) (string_of_type t2);
           unify_types t cs )
   
-  and subst (x : t) (v : t) (t : t) : t =
+  and subst (x : tvar) (v : t) (t : t) : t =
     match t with
     | Arrow (t1, t2) -> Arrow (subst x v t1, subst x v t2)
-    | _ -> if t = x then v else t
+    | TV tv -> if x = tv then v else t
+    | _ -> t
   
   and occurs (x : t) (t : t) : bool =
     match t with Arrow (t1, t2) -> occurs x t1 || occurs x t2 | _ -> t = x
@@ -104,10 +112,42 @@ let infer_type (expr : expr) =
     fun _ ->
       counter := !counter + 1;
       TV ("t" ^ string_of_int !counter)
-      
+
+  and generalize (ftv : tvar list) (t : t) : t =
+    match t with
+    | Arrow (t1, t2) -> Arrow (generalize ftv t1, generalize ftv t2)
+    | TV tv -> if List.mem tv ftv then TV tv else GV tv
+    | _ -> t
+
+  and free_type_variables (ts : t list) =
+    match ts with
+    | [] -> []
+    | Arrow (t1, t2) :: ts' -> free_type_variables (t1 :: t2 :: ts')
+    | TV tv :: ts' -> tv :: free_type_variables ts'
+    | _ :: ts' -> free_type_variables ts'
+  
+  and instantiate (t : t) =
+    let rec aux t instd =
+      match t with
+      | Arrow (t1, t2) ->
+          let t1', instd' = aux t1 instd in
+          let t2', instd'' = aux t2 instd' in
+          (Arrow (t1', t2'), instd'')
+      | GV gv -> handle gv instd instd
+      | _ -> (t, instd)
+    and handle gv instd original =
+      match instd with
+      | [] ->
+          let ntv = newTV () in
+          (ntv, (gv, ntv) :: original)
+      | (gv', tv') :: instd' ->
+          if gv' = gv then (tv', original) else handle gv instd' original
+    in
+    fst (aux t [])
+  
   in
   let gamma, t, cs = gather_constraints [] expr [] in
-  let t' = unify_types t cs in
+  let t' = generalize (free_type_variables (List.map snd gamma)) (unify_types t cs) in
   Printf.printf "Type of %s is %s" (string_of_expr expr) (string_of_type t');
   if gamma = [] then Printf.printf "\n"
   else (
@@ -145,6 +185,7 @@ let some_terms =
     App
       ( Fun ("fix", "f", App (V "f", App (V "fix", V "f"))),
         Lam ("x", Lam ("y", Lam ("z", I 2))) );
+    Let ("id", Lam ("x", V "x"), App (V "id", V "id"));
   ]
 
 let _ = List.map infer_type some_terms
