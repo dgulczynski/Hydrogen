ba54dddb89e1f9586c39bade27af654340ac0d88
dgulczynski <dominik.gulczynski@gmail.com>
2020-05-16 20:47:15 +0200

    Rename and restructure

--- hydrogen.ml
+++ hydrogen.ml
@@ -51,126 +51,125 @@ let rec string_of_expr : expr -> string = function
       aux e1 ^ " " ^ aux e2
   | Annoted (e, t)    -> "(" ^ string_of_expr e ^ " : " ^ string_of_type t ^ ")"
 
+let rec occurs (x : identifier) : typ -> bool = function
+  | Arrow (t1, t2)        -> occurs x t1 || occurs x t2
+  | TV {contents= Free a} -> x = a
+  | _                     -> false
+
+let rec find : typ -> typ = function
+  | Arrow (t1, t2)                 -> Arrow (find t1, find t2)
+  | TV ({contents= Bound t} as tv) ->
+      let t' = find t in
+      tv := Bound t' ;
+      t'
+  | t                              -> t
+
+let rec union ((t1, t2) : typ * typ) : unit =
+  match (find t1, find t2) with
+  | t1', t2' when t1' = t2' -> ()
+  | Arrow (a1, b1), Arrow (a2, b2) ->
+      union (a1, a2) ;
+      union (b1, b2)
+  | TV {contents= Bound t}, t' | t', TV {contents= Bound t} -> union (t, t')
+  | TV ({contents= Free a} as tv), t' | t', TV ({contents= Free a} as tv) ->
+      if occurs a t' then
+        raise (IllTyped ("The type variable " ^ a ^ " occurs inside " ^ string_of_type t'))
+      else tv := Bound t'
+  | t1', t2' ->
+      raise (IllTyped ("Cannot unify " ^ string_of_type t1' ^ " with " ^ string_of_type t2'))
+
+let type_of_var (gamma : env) (v : var) : typ =
+  match List.assoc_opt v gamma with
+  | None   -> raise (IllTyped ("Free variable " ^ v))
+  | Some t -> find t
+
+let (freshTV : unit -> typ), (refreshTV : unit -> unit) =
+  let counter = ref (int_of_char 'a' - 1) in
+  ( (fun () ->
+      incr counter ;
+      TV (ref (Free (Printf.sprintf "_%c" (char_of_int !counter)))))
+  , fun () -> counter := int_of_char 'a' - 1 )
+
+let unify_types (t : typ) (cs : (typ * typ) list) : typ = List.iter union cs ; find t
+
+let generalize (gamma : env) (t : typ) : typ =
+  let rec generalize' ftv = function
+    | Arrow (t1, t2) -> Arrow (generalize' ftv t1, generalize' ftv t2)
+    | TV ({contents= Free a} as tv) as t ->
+        if List.mem a ftv then t
+        else (
+          tv := Bound (freshGV ()) ;
+          t )
+    | TV {contents= Bound t} -> generalize' ftv t
+    | t -> t
+  and free_type_variables = function
+    | []      -> []
+    | t :: ts ->
+    match find t with
+    | Arrow (t1, t2)        -> free_type_variables (t1 :: t2 :: ts)
+    | TV {contents= Free a} -> a :: free_type_variables ts
+    | _                     -> free_type_variables ts
+  and freshGV =
+    let counter = ref (int_of_char 'a' - 1) in
+    fun _ ->
+      counter := !counter + 1 ;
+      GV (Printf.sprintf "%c" (char_of_int !counter))
+  in
+  generalize' (free_type_variables (List.map snd gamma)) t
+
+let instantiate (t : typ) : typ =
+  let rec instantiate' t instd =
+    match t with
+    | Arrow (t1, t2) ->
+        let t1', instd' = instantiate' t1 instd in
+        let t2', instd'' = instantiate' t2 instd' in
+        (Arrow (t1', t2'), instd'')
+    | GV gv          -> (
+      match List.assoc_opt gv instd with
+      | Some t -> (t, instd)
+      | None   ->
+          let ntv = freshTV () in
+          (ntv, (gv, ntv) :: instd) )
+    | _              -> (t, instd)
+  in
+  fst (instantiate' t [])
+
 let infer_type (expr : expr) =
-  let rec infer (gamma : env) (t : typ) : expr -> env = function
+  let rec inferM (gamma : env) (t : typ) : expr -> env = function
     | I _               ->
         union (t, Int) ;
         gamma
     | V v               ->
         union (t, instantiate (type_of_var gamma v)) ;
         gamma
-    | Lam (x, e)        -> infer gamma t (TLam (x, freshTV (), e))
+    | Lam (x, e)        -> inferM gamma t (TLam (x, freshTV (), e))
     | TLam (x, tx, e)   ->
         let te = freshTV () in
         union (t, Arrow (tx, te)) ;
-        let _ = infer ((x, tx) :: gamma) te e in
+        let _ = inferM ((x, tx) :: gamma) te e in
         gamma
     | Fun (f, x, e)     ->
-        let _ = infer ((f, t) :: gamma) t (Lam (x, e)) in
+        let _ = inferM ((f, t) :: gamma) t (Lam (x, e)) in
         gamma
     | TFun (f, x, t, e) ->
-        let _ = infer ((f, t) :: gamma) t (Lam (x, e)) in
+        let _ = inferM ((f, t) :: gamma) t (Lam (x, e)) in
         gamma
     | Let (x, e, e')    ->
         let te = freshTV () in
-        let _ = infer gamma te e in
-        infer ((x, generalize gamma (find te)) :: gamma) t e'
+        let _ = inferM gamma te e in
+        inferM ((x, generalize gamma (find te)) :: gamma) t e'
     | App (e1, e2)      ->
         let t2 = freshTV () in
-        let _ = infer gamma (Arrow (t2, t)) e1 in
-        infer gamma t2 e2
+        let _ = inferM gamma (Arrow (t2, t)) e1 in
+        inferM gamma t2 e2
     | Annoted (e, t')   ->
-        let _ = infer gamma t' e in
+        let _ = inferM gamma t' e in
         union (t, t') ;
         gamma
-  
-  and type_of_var (gamma : env) (v : var) : typ =
-    match List.assoc_opt v gamma with
-    | None   -> raise (IllTyped ("Free variable " ^ v))
-    | Some t -> find t
-  
-  and find : typ -> typ = function
-    | Arrow (t1, t2)                 -> Arrow (find t1, find t2)
-    | TV ({contents= Bound t} as tv) ->
-        let t' = find t in
-        tv := Bound t' ;
-        t'
-    | t                              -> t
-  
-  and union ((t1, t2) : typ * typ) : unit =
-    match (find t1, find t2) with
-    | t1', t2' when t1' = t2' -> ()
-    | Arrow (a1, b1), Arrow (a2, b2) ->
-        union (a1, a2) ;
-        union (b1, b2)
-    | TV {contents= Bound t}, t' | t', TV {contents= Bound t} -> union (t, t')
-    | TV ({contents= Free a} as tv), t' | t', TV ({contents= Free a} as tv) ->
-        if occurs a t' then
-          raise (IllTyped ("The type variable " ^ a ^ " occurs inside " ^ string_of_type t'))
-        else tv := Bound t'
-    | t1', t2' ->
-        raise
-          (IllTyped ("Cannot unify " ^ string_of_type t1' ^ " with " ^ string_of_type t2'))
-  
-  and occurs (x : identifier) : typ -> bool = function
-    | Arrow (t1, t2)        -> occurs x t1 || occurs x t2
-    | TV {contents= Free a} -> x = a
-    | _                     -> false
-  
-  and freshTV : unit -> typ =
-    let counter = ref (int_of_char 'a' - 1) in
-    fun _ ->
-      counter := !counter + 1 ;
-      TV (ref (Free (Printf.sprintf "_%c" (char_of_int !counter))))
-  
-  and unify_types (t : typ) (cs : (typ * typ) list) : typ = List.iter union cs ; find t
-  
-  and generalize (gamma : env) (t : typ) : typ =
-    let rec generalize' ftv = function
-      | Arrow (t1, t2) -> Arrow (generalize' ftv t1, generalize' ftv t2)
-      | TV ({contents= Free a} as tv) as t ->
-          if List.mem a ftv then t
-          else (
-            tv := Bound (freshGV ()) ;
-            t )
-      | TV {contents= Bound t} -> generalize' ftv t
-      | t -> t
-    and free_type_variables = function
-      | []      -> []
-      | t :: ts ->
-      match find t with
-      | Arrow (t1, t2)        -> free_type_variables (t1 :: t2 :: ts)
-      | TV {contents= Free a} -> a :: free_type_variables ts
-      | _                     -> free_type_variables ts
-    and freshGV =
-      let counter = ref (int_of_char 'a' - 1) in
-      fun _ ->
-        counter := !counter + 1 ;
-        GV (Printf.sprintf "%c" (char_of_int !counter))
-    in
-    generalize' (free_type_variables (List.map snd gamma)) t
-  
-  and instantiate (t : typ) : typ =
-    let rec instantiate' t instd =
-      match t with
-      | Arrow (t1, t2) ->
-          let t1', instd' = instantiate' t1 instd in
-          let t2', instd'' = instantiate' t2 instd' in
-          (Arrow (t1', t2'), instd'')
-      | GV gv          -> (
-        match List.assoc_opt gv instd with
-        | Some t -> (t, instd)
-        | None   ->
-            let ntv = freshTV () in
-            (ntv, (gv, ntv) :: instd) )
-      | _              -> (t, instd)
-    in
-    fst (instantiate' t [])
-  
   in
   try
     let t = TV (ref (Free "_")) in
-    let gamma = infer [] t expr in
+    let gamma = inferM [] t expr in
     (gamma, find t)
   with IllTyped e ->
     print_string ("Type inference error: " ^ e ^ "\n") ;
